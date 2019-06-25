d2c88d61f318b9091a61c7b7d06a4b30f69a9376
unknown <trueteller89@gmail.com>
2019-06-25 08:48:37 +0300

    remove extra code

--- public/app.js
+++ public/app.js
@@ -13,7 +13,6 @@ angular.module('myApp', ['ngMaterial', 'md.data.table'])
 		$scope.films = data.data.dataFilms
 		$scope.residents = data.data.dataRes
 		$scope.planets.forEach(function(el) {
-			el.isExpand = false
 			el.residentNames =[]
 			el.residents.forEach(r => {
 				el.residentNames.push($scope.residents.filter(item =>item.url === r)[0].name)
@@ -23,32 +22,29 @@ angular.module('myApp', ['ngMaterial', 'md.data.table'])
 				el.filmNames.push($scope.films.filter(item =>item.url === r)[0].title)
 			})
 		})
-		$scope.showDetails = function($index) {
-			if ($scope.selected) {
-				if ($scope.selected !== $index) {
-					$scope.selected = $index
-				} else {
-					$scope.selected = -1} 
-				}
-				else {
-					$scope.selected = $index
-				}
+	})
+	$scope.showDetails = function($index) {
+		if ($scope.selected) {
+			if ($scope.selected !== $index) {
+				$scope.selected = $index
+			} else {
+				$scope.selected = -1} 
 			}
-		})
-	$scope.expand = function(planet) {
-		planet.isExpand = !planet.isExpand
-	}
-}]).filter('thousandSuffix', function () {
-	return function (input, decimals) {
-		var exp, rounded,
-		suffixes = ['k', 'm', 'g', 't', 'p', 'e'];
-		if(window.isNaN(input)) {
-			return null;
+			else {
+				$scope.selected = $index
+			}
 		}
-		if(input < 1000) {
-			return input;
-		}
-		exp = Math.floor(Math.log(input) / Math.log(1000));
-		return (input / Math.pow(1000, exp)).toFixed(decimals) + suffixes[exp - 1];
-	};
-});
+	}]).filter('thousandSuffix', function () {
+		return function (input, decimals) {
+			var exp, rounded,
+			suffixes = ['k', 'm', 'g', 't', 'p', 'e'];
+			if(window.isNaN(input)) {
+				return null;
+			}
+			if(input < 1000) {
+				return input;
+			}
+			exp = Math.floor(Math.log(input) / Math.log(1000));
+			return (input / Math.pow(1000, exp)).toFixed(decimals) + suffixes[exp - 1];
+		};
+	});
